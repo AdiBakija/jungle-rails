f48cf955de382182f9a9ea382fe8f815fe1c34b4
Adi <adibakija35@hotmail.com>
2018-11-08 01:53:53 +0000

    Completed soft user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -11,6 +19,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
 
   namespace :admin do
     root to: 'dashboard#show'
---
+++ db/migrate/20181108014815_add_fields_to_users.rb
@@ -0,0 +1,5 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :lastname, :string
+  end
+end
